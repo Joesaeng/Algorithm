13f89629644a5b2aa90611a347372c34f573f7d4
Jo Seongmin <ss662717@naver.com>
2022-09-04 16:26:29 +0900

    0904
    1167 트리의 지름
    1967 트리의 지름
    // 트리의 지름은 임의의 정점 x에서 가장 먼 정점 v1을 찾은 뒤, v1에서 가장 먼 정점 v2를 찾아서 v1과 v2의 경로를 구하면 그것이 그 트리의 지름이다//

--- Algorithm/1967.cpp
+++ Algorithm/1967.cpp
@@ -1,12 +1,13 @@
-// BOJ 1167 :: 트리의 지름
+// BOJ 1967 :: 트리의 지름
 #include <bits/stdc++.h>
 #define FASTIO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define MAX 10001
 using namespace std;
-int n, a, b, c;
-vector<pair<int, int>> vec[100001];
-bool vi[100001];
-int ans = 0;
+int  n, a, b, c;
+vector<pair<int, int>> vec[MAX];
+bool vi[MAX];
 int far = 0;
+int ans = 0;
 void DFS(int vt,int cost)
 {
 	if (cost > ans)
@@ -18,31 +19,24 @@ void DFS(int vt,int cost)
 	for (int i = 0; i < vec[vt].size(); ++i)
 	{
 		int next = vec[vt][i].first;
-		if (vi[next]) continue;
-		DFS(next,cost + vec[vt][i].second);
+		if (vi[next])continue;
+		DFS(next, cost + vec[vt][i].second);
 	}
 	vi[vt] = false;
 }
-void input(int num)
-{
-	cin >> b;
-	if (b == -1) return;
-	cin >> c;
-	vec[num].push_back({ b,c });
-	input(num);
-}
+
 int main()
 {
 	FASTIO;
 	cin >> n;
-	for (int i = 0; i < n; ++i)
+	for (int i = 1; i < n; ++i)
 	{
-		cin >> a;
-		input(a);
+		cin >> a >> b >> c;
+		vec[a].push_back({ b,c });
+		vec[b].push_back({ a,c });
 	}
-	DFS(1,0);
+	DFS(1, 0);
 	DFS(far, 0);
-
 	cout << ans;
 	return 0;
 }
